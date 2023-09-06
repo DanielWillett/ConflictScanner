e6849283fe06861aef6c3a784d7920aba66a9e1a
Danny Willett <dannywill8@gmail.com>
2023-09-06 17:54:20 -0400

    Add project files.

---
+++ Assets/AssetReader.cs
@@ -0,0 +1,125 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UnturnedWorkshopAnalyst.Assets;
+internal class AssetReader
+{
+    private readonly DatParser _parser = new DatParser();
+
+    public List<FileAsset> Scan(string folder)
+    {
+        lock (_parser)
+        {
+            List<FileAsset> rtn = new List<FileAsset>(32);
+            DirectoryInfo dir = new DirectoryInfo(folder);
+            foreach (FileInfo info in dir.EnumerateFiles("*", SearchOption.AllDirectories))
+            {
+                if (info.Extension.Equals(".asset", StringComparison.Ordinal))
+                {
+                    string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                    if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                    {
+                        if (File.Exists(Path.Combine(dirName, dirName + ".asset")) ||
+                            File.Exists(Path.Combine(dirName, dirName + ".dat")) ||
+                            File.Exists(Path.Combine(dirName, "Asset.dat")))
+                            continue;
+                        
+                    }
+                }
+                else if (info.Extension.Equals(".dat", StringComparison.Ordinal))
+                {
+                    string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                    if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                        continue;
+                }
+                else continue;
+                DatDictionary dict = ReadFile(info.FullName);
+                FileAsset? asset = TryRead(dict, info);
+                if (asset != null)
+                {
+                    rtn.Add(asset);
+                }
+            }
+            Console.WriteLine($"Assets discovered: {rtn.Count}.");
+
+            return rtn;
+        }
+    }
+    public FileAsset? TryRead(DatDictionary dictionary, FileInfo file)
+    {
+        Guid guid;
+        ushort id = 0;
+        Type? assetType = null;
+        if (dictionary.TryGetDictionary("Metadata", out DatDictionary metadata))
+        {
+            if (!metadata.TryParseGuid("Guid", out guid))
+            {
+                ReportError(file, "Missing Metadata.Guid property.");
+                return null;
+            }
+            assetType = metadata.ParseType("Type");
+            if (assetType == null)
+            {
+                ReportError(file, "Missing Metadata.Type property.");
+                return null;
+            }
+        }
+        else
+        {
+            dictionary.TryParseGuid("GUID", out guid);
+            dictionary.TryParseUInt16("ID", out id);
+        }
+        if (guid == Guid.Empty)
+        {
+            ReportError(file, "GUID was empty.");
+            return null;
+        }
+        if (assetType == null)
+        {
+            if (!dictionary.TryGetString("Type", out string typeStr))
+            {
+                ReportError(file, "Missing Type property.");
+                return null;
+            }
+
+            assetType = SDG.Unturned.Assets.assetTypes.getType(typeStr);
+
+            if (assetType == null)
+            {
+                assetType = dictionary.ParseType("Type");
+
+                if (assetType == null)
+                {
+                    ReportError(file, $"Unknown value: \"{typeStr}\" for Type property.");
+                    return null;
+                }
+            }
+        }
+
+        if (!typeof(Asset).IsAssignableFrom(assetType))
+        {
+            ReportError(file, $"Type {assetType.FullName} is not assignable from Asset.");
+            return null;
+        }
+
+        FileAsset asset = new FileAsset(assetType, file, guid, id, dictionary);
+        asset.CacheLocal(_parser);
+        return asset;
+    }
+    private void ReportError(FileInfo file, string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Asset \"{Path.GetFileNameWithoutExtension(file.FullName)}\" load error: \"{message}\".{Environment.NewLine}\tFile: {file.FullName}");
+        Console.ResetColor();
+    }
+
+    private DatDictionary ReadFile(string path)
+    {
+        using FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using StreamReader inputReader = new StreamReader(fileStream);
+        // ReSharper disable once InconsistentlySynchronizedField
+        return _parser.Parse(inputReader);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using UnturnedWorkshopAnalyst.Assets;
+using System.Windows.Forms;
+
+namespace ConflictScanner;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        string? path = args.Length > 0 ? args[0] : null;
+        if (path == null || !Uri.TryCreate(path, UriKind.Absolute, out Uri uri))
+        {
+            Console.WriteLine("Enter absolute root folder path:");
+            while (true)
+            {
+                try
+                {
+                    uri = new Uri(Console.ReadLine()!, UriKind.Absolute);
+                    break;
+                }
+                catch (UriFormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid path.");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+        UnturnedNexus nexus = new UnturnedNexus();
+        nexus.initialize();
+        AssetReader reader = new AssetReader();
+        while (true)
+        {
+            List<FileAsset> assets = reader.Scan(Uri.UnescapeDataString(uri.AbsolutePath));
+            Dictionary<EAssetType, Dictionary<ushort, List<FileAsset>>> duplicateIds = new Dictionary<EAssetType, Dictionary<ushort, List<FileAsset>>>();
+            Dictionary<Guid, List<FileAsset>> duplicateGuids = new Dictionary<Guid, List<FileAsset>>();
+            List<FileAsset> noGuid = new List<FileAsset>();
+            for (int i = 0; i < assets.Count; ++i)
+            {
+                FileAsset file = assets[i];
+                if (file.Category != EAssetType.NONE && file.Id > 0)
+                {
+                    if (duplicateIds.TryGetValue(file.Category, out Dictionary<ushort, List<FileAsset>> duplicateIdsInCategory))
+                    {
+                        if (duplicateIdsInCategory.TryGetValue(file.Id, out List<FileAsset> list))
+                            list.Add(file);
+                        else duplicateIdsInCategory.Add(file.Id, new List<FileAsset> { file });
+                    }
+                    else duplicateIds.Add(file.Category, new Dictionary<ushort, List<FileAsset>> { { file.Id, new List<FileAsset> { file } } });
+                }
+
+                if (file.Guid == Guid.Empty)
+                {
+                    noGuid.Add(file);
+                }
+                else if (duplicateGuids.TryGetValue(file.Guid, out List<FileAsset> files))
+                    files.Add(file);
+                else
+                    duplicateGuids.Add(file.Guid, new List<FileAsset> { file });
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Operation? (free <start id> <category>, guid, conflicts, exit/quit)");
+            string? input = Console.ReadLine();
+            if (input == null)
+                return;
+            if (input.Equals("conflicts", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if (noGuid.Count > 0)
+                {
+                    Console.WriteLine("No GUID:");
+                    foreach (FileAsset file in noGuid)
+                    {
+                        Console.WriteLine(" " + file.AssetName);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("   " + Uri.UnescapeDataString(uri.MakeRelativeUri(new Uri(file.File.FullName, UriKind.Absolute)).ToString()));
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Duplicate GUIDs:");
+                foreach (List<FileAsset> files in duplicateGuids.Values.Where(x => x.Count > 1))
+                {
+                    Console.WriteLine(" " + files[0].Guid);
+                    foreach (FileAsset file in files)
+                    {
+                        Console.WriteLine("   " + file.AssetName);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("   " + Uri.UnescapeDataString(uri.MakeRelativeUri(new Uri(file.File.FullName, UriKind.Absolute)).ToString()));
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Duplicate IDs:");
+                foreach (List<FileAsset> files in duplicateIds.Values.SelectMany(x => x.Values).Where(x => x.Count > 1))
+                {
+                    Console.WriteLine(" " + files[0].Id + " @ " + files[0].Category);
+                    foreach (FileAsset file in files)
+                    {
+                        Console.WriteLine("   " + file.AssetName);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("   " + Uri.UnescapeDataString(uri.MakeRelativeUri(new Uri(file.File.FullName, UriKind.Absolute)).ToString()));
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Done");
+            }
+            else if (input.StartsWith("free", StringComparison.InvariantCultureIgnoreCase))
+            {
+                int arg0 = input.IndexOf(' ');
+                if (arg0 == -1 || arg0 == input.Length - 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("free <category> <start id>");
+                    continue;
+                }
+
+                int arg1 = input.IndexOf(' ', arg0 + 1);
+                if (arg1 == -1 || arg1 == input.Length - 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("free <category> <start id>");
+                    continue;
+                }
+
+                if (!Enum.TryParse(input.Substring(arg0 + 1, arg1 - arg0 - 1), true, out EAssetType type) || !ushort.TryParse(input.Substring(arg1 + 1), NumberStyles.Number, CultureInfo.InvariantCulture, out ushort id))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("free <category: EAssetType> <start id: UInt16>");
+                    continue;
+                }
+
+                if (!duplicateIds.TryGetValue(type, out Dictionary<ushort, List<FileAsset>> dict))
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine($"Free {type.ToString().ToLowerInvariant()} id: {id} (Copied).");
+                    Clipboard.SetText(id.ToString(CultureInfo.InvariantCulture));
+                    continue;
+                }
+
+                while (id < ushort.MaxValue)
+                {
+                    if (!dict.ContainsKey(id))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine($"Free {type.ToString().ToLowerInvariant()} id: {id} (Copied).");
+                        Clipboard.SetText(id.ToString(CultureInfo.InvariantCulture));
+                        break;
+                    }
+
+                    ++id;
+                }
+
+                if (id == ushort.MaxValue)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"No free {type} ids.");
+                }
+            }
+            else if (input.Equals("guid", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Guid guid;
+                do
+                {
+                    guid = Guid.NewGuid();
+                } while (duplicateGuids.ContainsKey(guid));
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine($"Free GUID: \"{guid:N}\" (Copied).");
+                Clipboard.SetText(guid.ToString("N"));
+            }
+            else if (input.Equals("quit", StringComparison.InvariantCultureIgnoreCase) ||
+                input.Equals("exit", StringComparison.InvariantCultureIgnoreCase)) break;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press [ENTER] to proceed.");
+
+            input = Console.ReadLine();
+            if (input == null)
+                return;
+            if (input.Equals("quit", StringComparison.InvariantCultureIgnoreCase) ||
+                input.Equals("exit", StringComparison.InvariantCultureIgnoreCase)) break;
+        }
+
+        nexus.shutdown();
+        Console.ReadLine();
+    }
+}
